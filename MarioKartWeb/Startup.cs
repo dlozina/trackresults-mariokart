be6d4df2ffdfe9b1259941d6f64cfdd2b68ddc70
dlozina <dino.lozina@itsistemi.com>
2020-05-10 14:28:16 +0200

    F8 - added log in page and admin session control

---
+++ MarioKartWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarioKartWeb.Startup))]
+namespace MarioKartWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
